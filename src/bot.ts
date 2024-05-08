2e085d9a5b54b8ad726ed5fefd0921ccfd34a255
ChristianSantanaLlanes <82761124+ChristianSantanaLlanes@users.noreply.github.com>
2024-05-07 22:47:56 -0500

    ...

--- src/bot.ts
+++ src/bot.ts
@@ -1,14 +1,53 @@
-import { Bot, InlineKeyboard, webhookCallback } from "grammy";
-import { chunk } from "lodash";
+import { Bot, webhookCallback, session, SessionFlavor, Context } from "grammy";
 import express from "express";
-import { applyTextEffect, Variant } from "./textEffects";
 
-import type { Variant as TextEffectVariant } from "./textEffects";
+//Performance and Limits
+import { autoRetry } from "@grammyjs/auto-retry";
+import { apiThrottler } from "@grammyjs/transformer-throttler";
+import { limit } from "@grammyjs/ratelimiter";
 
+//Storage
+import { freeStorage } from "@grammyjs/storage-free";
+
+//Extra
+import { hydrate, HydrateFlavor } from "@grammyjs/hydrate";
+import { I18n, I18nFlavor } from "@grammyjs/i18n";
+
+import { start } from "./commands/start";
+
+interface SessionData {
+  nombre: string;
+}
+
+export type MyContext = HydrateFlavor<Context> & SessionFlavor<SessionData> & I18nFlavor;
 // Create a bot using the Telegram token
-const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
+export const bot = new Bot<MyContext>(process.env.TELEGRAM_TOKEN || "");
 
-bot.command('start', (ctx) => ctx.reply('Hola'))
+function initial(): SessionData {
+  return { nombre: '' };
+}
+
+const i18n = new I18n<MyContext>({
+  defaultLocale: "es", // ver más abajo para más información
+
+  // Cargar todos los archivos de traducción de locales/.
+  directory: "locales",
+});
+
+const throttler = apiThrottler();
+bot.api.config.use(throttler);
+bot.api.config.use(autoRetry());
+
+bot.use(limit());
+bot.use(i18n);
+bot.use(session({ 
+  initial, 
+  storage: freeStorage<SessionData>(bot.token),
+ }));
+ bot.use(hydrate());
+
+//Comandos
+bot.command('start', async (ctx) => start(ctx));
 
 // Start the server
 if (process.env.NODE_ENV === "production") {
---
+++ src/commands/start.ts
@@ -0,0 +1,7 @@
+import { MyContext } from "../bot";
+
+export async function start(ctx: MyContext) {
+  ctx.session.nombre = ctx.from?.first_name ?? ''
+  await ctx.reply(ctx.t('welcome', {name: ctx.session.nombre}));
+}
+

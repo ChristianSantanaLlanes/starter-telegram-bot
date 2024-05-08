44f9373a3830f5d0f82c135e044a75bd3b124853
ChristianSantanaLlanes <82761124+ChristianSantanaLlanes@users.noreply.github.com>
2024-05-07 22:48:40 -0500

    init

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -3,5 +3,6 @@ import { MyContext } from "../bot";
 export async function start(ctx: MyContext) {
   ctx.session.nombre = ctx.from?.first_name ?? ''
   await ctx.reply(ctx.t('welcome', {name: ctx.session.nombre}));
+  
 }
 
